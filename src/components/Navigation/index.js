261df26889839efe8003e2181d62d70d855aa1e7
Eric Crain <ericcrain77@gmail.com>
2022-01-08 15:36:19 -0500

    adding a list item to the nav in the header

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Navigation() {
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                  <a href="#about">About Me</a>
+                </li>
+                <li>
+                  <a href='#skills'>Developer Skills</a>
+                </li>
+                <li>
+                  <a href="#projects">Projects</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact Me</a>
+                </li>
+              </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
